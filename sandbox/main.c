64295e2caa3a837637de9d1fe999ea0849282597
Clement Monier <clement.monier@bodet-timesport.com>
2018-07-11 14:12:57 +0200

    Add communication sandbox tests for Ubuntu

---
+++ sandbox/main.c
@@ -0,0 +1,40 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "communication/lvl1SocketCommunications.h"
+
+#define TRUE				1
+#define FALSE				0
+
+/* definition of signal handler */
+static void interrupt_handler(int status);
+
+static int run = 1;
+
+int main(int argc, char *argv[]) {
+	struct sigaction saio;
+
+
+	/* Define interrupt handler */
+	saio.sa_handler = interrupt_handler;
+	sigemptyset(&saio.sa_mask);
+	saio.sa_flags = 0;
+
+	/* For ctrl^C interruption */
+	sigaction(SIGINT, &saio, 0);
+
+        com_start_server();
+	while(run);
+        com_stop_server();
+
+	return 0;
+}
+
+/* Interrupt signal handler */
+static void interrupt_handler(int status) {
+	if (status == SIGINT) {
+            run = FALSE;
+	}
+}
+
