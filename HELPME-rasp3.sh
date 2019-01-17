90e528f6361bc363794a0d065f96396b6449f361
clem-on <clement.monier@outlook.com>
2019-01-17 14:01:17 +0100

    Update HELPME-rasp3.sh

--- HELPME-rasp3.sh
+++ HELPME-rasp3.sh
@@ -8,3 +8,18 @@ ssh-copy-id pi@192.168.1.10
 
 admin
 mdp habituel
+
+raspberry pi3 pinout :
+http://pi4j.com/images/j8header-3b.png
+
+raspberry pi3 static ip :
+https://www.ionos.co.uk/digitalguide/server/configuration/provide-raspberry-pi-with-a-static-ip-address/
+
+se connecter à un point d'accès WiFi en cmd line :
+https://raspbian-france.fr/connecter-wifi-raspberry-pi-3/
+
+bennetout at startup :
+https://www.dexterindustries.com/howto/run-a-program-on-your-raspberry-pi-at-startup/#systemd
+
+hotspot wifi + serveur web :
+https://raspbian-france.fr/creer-un-hotspot-wi-fi-en-moins-de-10-minutes-avec-la-raspberry-pi/
