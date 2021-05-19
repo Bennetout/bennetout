5e14ee6ab8baef4dad793b78c492496bd2aa7b0a
Clement Monier <clement.monier@outlook.com>
2021-05-19 17:53:52 +0200

    clean old app and toolchain

--- app-raspberry/communication/lvl1SocketCommunications.c
+++
@@ -1,9 +0,0 @@
-#include "lvl1SocketCommunications.h"
-
-int com_start_server(void) {
-
-}
-
-int com_stop_server(void) {
-
-}
--- app-raspberry/rotation-sensors/rotation-sensors.h
+++
@@ -1,5 +0,0 @@
-void rot_init(void);
-
-void rot_destroy(void);
-
-float rot_get_value(int channel);
