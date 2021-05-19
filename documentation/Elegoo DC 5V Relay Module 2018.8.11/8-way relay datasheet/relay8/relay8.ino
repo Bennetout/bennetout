596dd46cad2856792c53a28ee4bbaec059d6e308
Clement Monier <clement.monier@outlook.com>
2021-05-19 17:56:00 +0200

    Add documentation about actual pinout and relays

---
+++ documentation/Elegoo DC 5V Relay Module 2018.8.11/8-way relay datasheet/relay8/relay8.ino
@@ -0,0 +1,49 @@
+void setup()
+{   pinMode(11,OUTPUT);
+    pinMode(10,OUTPUT);
+pinMode(9,OUTPUT);
+pinMode(8,OUTPUT);
+pinMode(7,OUTPUT);
+pinMode(6,OUTPUT);
+pinMode(5,OUTPUT);
+pinMode(4,OUTPUT);
+
+}
+
+ void loop()
+{ 
+  digitalWrite(11,LOW);
+    delay(2000);
+    digitalWrite(10, LOW);
+delay(2000);
+digitalWrite(9, LOW);
+delay(2000);
+digitalWrite(8, LOW);
+delay(2000);
+digitalWrite(7, LOW);
+delay(2000);
+digitalWrite(6, LOW);
+delay(2000);
+digitalWrite(5, LOW);
+delay(2000);
+digitalWrite(4, LOW);
+delay(2000);
+
+digitalWrite(11, HIGH);
+delay(4000);
+digitalWrite(10, HIGH);
+delay(4000);
+digitalWrite(9, HIGH);
+delay(4000);
+digitalWrite(8, HIGH);
+delay(4000);
+digitalWrite(7, HIGH);
+delay(4000);
+digitalWrite(6, HIGH);
+delay(4000);
+digitalWrite(5, HIGH);
+delay(4000);
+digitalWrite(4, HIGH);
+delay(4000);
+}
+
